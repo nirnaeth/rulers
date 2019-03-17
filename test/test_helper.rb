7b71864b2fbf3cf8ee0cf56605a69ab7ca5853df
Monica Giambitto <m.giambitto@freegoweb.it>
2019-03-17 16:56:32 +0100

    use rack/test and test/unit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "rulers"
+require "rack/test"
+require "test/unit"
 
-require "minitest/autorun"
+# Always use local Rulers first
+# `expand_path` so it's absolute.
+# this is important if something changes the current directory
+# `unshift` to prepend.
+dir = File.join(File.dirname(__FILE__), "..", "lib")
+$LOAD_PATH.unshift File.expand_path(dir)
+
+require "rulers"
